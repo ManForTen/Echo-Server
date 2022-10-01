fef5149735ef39d8823ce090f704b17fc5c39381
Artem Petrikov <manforten@tandex.ru>
2022-10-02 01:01:28 +0300

    New changes in server.py

--- server.py
+++ server.py
@@ -3,17 +3,18 @@ import socket
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(0)
+print('Успешное подключение!')
 conn, addr = sock.accept()
 print(addr)
-
 msg = ''
 
 while True:
-	print('Прием данных')
+	print('Прием данных:')
 	try:
 		data = conn.recv(1024)
-	except (ConnectionAbortedError):
+	except (ConnectionAbortedError, ConnectionResetError):
 		print('Ошибка!')
+
 	msg = data.decode()
 	print(msg)
 	if msg == 'shutdown':
@@ -26,3 +27,4 @@ while True:
 
 
 conn.close()
+print('Отключение клиента!')
