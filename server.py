f8c64d60726c2bb97271d58209c0c8b1c84516d1
Artem Petrikov <manforten@tandex.ru>
2022-10-02 21:25:20 +0300

    New changes in server.py, new server listening, even if the client disconnects

--- server.py
+++ server.py
@@ -5,11 +5,12 @@ sock.bind(('', 9090))
 sock.listen(1)
 print('Успешное подключение!')
 print('Введите server off, для отключения сервера.')
-conn, addr = sock.accept()
-print(addr)
-msg = ''
+
 
 while True:
+	conn, addr = sock.accept()
+	print(addr)
+	msg = ''
 	print('Прием данных:')
 	try:
 		data = conn.recv(1024)
